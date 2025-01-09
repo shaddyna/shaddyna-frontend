76460d2bbd3d9773adffcc2323a490faa5ed8983
SAMUEopps <samuelhope805@gmail.com>
2025-01-09 05:46:26 +0300

    frontend modifications

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const HomePage: React.FC = () => {
         imageUrl='https://i.pinimg.com/736x/73/57/6e/73576e43a90f2a098311711b35d20dbe.jpg'
         //imageUrl="https://i.pinimg.com/736x/30/80/d8/3080d8d5f2c890c4f3fde3e9b1645449.jpg" // Replace with your image path
         buttonText="Shop Now"
-        buttonLink="/shop" // Replace with your shop link
+        buttonLink="/" // Replace with your shop link
       /> 
       
       <div className="block lg:hidden">
