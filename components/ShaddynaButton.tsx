de4c70224d2d9bc3c156f747576fcb64d643698d
SAMUEopps <samuelhope805@gmail.com>
2025-01-22 18:14:33 +0300

    more changes

--- components/ShaddynaButton.tsx
+++ components/ShaddynaButton.tsx
@@ -1,24 +1,31 @@
+"use client"
 import { FC } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation"; // Using correct import
 
 interface ShaddynaButtonProps {
   loading?: boolean;
 }
 
 const ShaddynaButton: FC<ShaddynaButtonProps> = ({ loading }) => {
+  const router = useRouter(); // Correct router hook import
+
+  const handleClick = () => {
+    router.push("/shaddyna"); // Handle navigation on click
+  };
+
   return (
-    <Link href="">
-      <button
-        className="w-full pt-2 px-6 py-2 border-2 border-[#182155] font-bold text-white bg-[#ff199c] hover:bg-[#182155] rounded transition lg:hidden"
-        disabled={loading}
-      >
-        {loading ? "Loading..." : "Shaddyna Hub"}
-      </button>
-    </Link>
+    <button
+      className="w-full pt-2 px-6 py-2 border-2 border-[#182155] font-bold text-white bg-[#ff199c] hover:bg-[#182155] rounded transition lg:hidden"
+      onClick={handleClick}
+      disabled={loading}
+    >
+      {loading ? "Loading..." : "Shaddyna Hub"}
+    </button>
   );
 };
 
 export default ShaddynaButton;
+
 
 
 
