82e7523ac218636834ab708c5484d7812c1c6061
SAMUEopps <samuelhope805@gmail.com>
2025-02-11 11:09:02 +0300

    first commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { FaInfoCircle, FaCogs, FaPhone } from 'react-icons/fa'; // Import icons
 
 const Header: React.FC = () => {
   return (
-    <header className="text-white p-2" style={{ backgroundColor: "#182155" }}>
+    <header className="text-white p-2" style={{ backgroundColor: "#ff199c" }}>
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="text-xl sm:text-xl font-semibold"> {/* Adjust logo size for small screens */}
@@ -19,31 +19,31 @@ const Header: React.FC = () => {
             {/* Use icons instead of text on small screens */}
             <li className="sm:hidden">
               <Link href="">
-                <FaInfoCircle className="text-pink-500" />
+                <FaInfoCircle className="text-white" />
               </Link>
             </li>
             <li className="sm:hidden">
               <Link href="">
-                <FaCogs className="text-pink-500" />
+                <FaCogs className="text-white" />
               </Link>
             </li>
             <li className="sm:hidden">
               <Link href="">
-                <FaPhone className="text-pink-500" />
+                <FaPhone className="text-white" />
               </Link>
             </li>
 
             {/* Text links for larger screens */}
             <li className="hidden sm:block">
-            <Link href="" className="text-[#ff199c] hover:text-white">About</Link>
+            <Link href="" className="text-white hover:text-white">About</Link>
 
 
             </li>
             <li className="hidden sm:block">
-              <Link href="" className="text-[#ff199c] hover:text-white">Services</Link>
+              <Link href="" className="text-white hover:text-white">Services</Link>
             </li>
             <li className="hidden sm:block">
-              <Link href="" className="text-[#ff199c] hover:text-white">Contact</Link>
+              <Link href="" className="text-white hover:text-white">Contact</Link>
             </li>
                       </ul>
         </nav>
