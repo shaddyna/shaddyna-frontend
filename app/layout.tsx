35d0118b7d940821cc2e11756d95c338a6f338b8
SAMUEopps <samuelhope805@gmail.com>
2025-02-21 19:20:13 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,7 +131,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
           {isVisible && (
             <button
-              className="fixed top-1 right-5 bg-blue-600 text-white px-4 py-2 rounded shadow-lg animate-bounce"
+              className="fixed top-1 right-5 bg-[#0f1c47] text-white px-4 py-2 rounded shadow-lg animate-bounce"
               onClick={handleInstallClick}
             >
               Install Shaddyna 🚀
